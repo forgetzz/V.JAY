8716f867daf71a7a9044164f72708028fd15d4ac
forgetzz <forget.noxa90@gmail.com>
2026-06-23 19:57:47 +0800

    new

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "../css/Navbar.css"
 import logo from "../../assets/logo.png";
 import scissor from "../../assets/scissor.png";
 import { Link } from "react-router-dom";
